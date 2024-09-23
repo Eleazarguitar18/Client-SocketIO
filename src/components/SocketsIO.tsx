4eddb08591d40123c5fa0e78267608eab63d960a
Eleazarguitar18 <eleazarguitar18@gmail.com>
2024-09-23 14:12:21 -0400

    primer commit sin sockets

---
+++ src/components/SocketsIO.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class SocketsIO extends Component {
+  render() {
+    return (
+      <div>SocketsIO</div>
+    )
+  }
+}
+
+export default SocketsIO
